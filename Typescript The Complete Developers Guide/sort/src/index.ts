18d50fac3341234db6536eac3a81a40382224784
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2023-12-01 12:08:38 -0300

    Add sort

---
+++ Typescript The Complete Developers Guide/sort/src/index.ts
@@ -0,0 +1,20 @@
+class Sorter {
+    constructor(public collection: number[]) {}
+
+    sort(): void {
+        const { length } = this.collection; // same as const length = this.collection.length;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
