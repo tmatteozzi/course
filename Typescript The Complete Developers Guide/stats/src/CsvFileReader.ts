4294f3cf9e0439af780ee1d7a1a2787a7aece24b
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2023-12-09 19:38:12 -0300

    Generic class

--- Typescript The Complete Developers Guide/stats/src/CsvFileReader.ts
+++ Typescript The Complete Developers Guide/stats/src/CsvFileReader.ts
@@ -1,15 +1,13 @@
 import fs from 'fs';
 
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-// MATCH DATA TUPLE
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CsvFileReader {
-    data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+    data: T[] = [];
 
     constructor(public filename: string) {}
+
+    // PARTICULAR CSV CODE
+    abstract mapRow(row: string[]): T;
 
     read(): void {
         this.data = fs
@@ -21,18 +19,5 @@ export class CsvFileReader {
                 return row.split(',');
             })
             .map(this.mapRow);
-    }
-
-    // PARTICULAR CSV CODE (FOOTBALL.CSV)
-    mapRow(row: string[]): MatchData {
-        return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6]
-        ];
     }
 }
