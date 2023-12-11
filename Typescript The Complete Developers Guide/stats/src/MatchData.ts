572cf131b862c8c9d3001340b4473b5093b36235
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2023-12-11 18:06:01 -0300

    Composition-Based Approach

---
+++ Typescript The Complete Developers Guide/stats/src/MatchData.ts
@@ -0,0 +1,12 @@
+import { MatchResult } from './MatchResult';
+
+// MATCH DATA TUPLE
+export type MatchData = [
+    Date,
+    string,
+    string,
+    number,
+    number,
+    MatchResult,
+    string
+];
--- Typescript The Complete Developers Guide/stats/src/index.ts
+++ Typescript The Complete Developers Guide/stats/src/index.ts
@@ -1,6 +1,8 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 // CREATE OBJECT THAT SATISFIES 'DATAREADER' INTERFACE
 const csvFileReader = new CsvFileReader('football.csv');
@@ -9,14 +11,10 @@ const matchReader = new MatchReader(csvFileReader);
 // LOAD READER
 matchReader.load();
 
-// ANALIZE HOW MANY TIMES DID MU WIN AT HOME
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++;
-    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-
-console.log(`Man United won: ${manUnitedWins} games`);
+// CREATE SUMMARY WITH WINS ANALYSIS AND CONSOLE REPORT
+const summary = new Summary(
+    new WinsAnalysis('Man United'),
+    new ConsoleReport()
+);
+// BUILD AND PRINT REPORT
+summary.buildAndPrintReport(matchReader.matches);
