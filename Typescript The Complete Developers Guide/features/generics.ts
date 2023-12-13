ab2944296bc274233a23ca56835e1ccbbdeb442a
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2023-12-13 18:28:48 -0300

    Generics

---
+++ Typescript The Complete Developers Guide/features/generics.ts
@@ -0,0 +1,69 @@
+class ArrayOfNumbers {
+    constructor(public collection: number[]) {}
+
+    get(index: number): number {
+        return this.collection[index];
+    }
+}
+
+class ArrayOfStrings {
+    constructor(public collection: string[]) {}
+
+    get(index: number): string {
+        return this.collection[index];
+    }
+}
+
+class ArrayOfAnything<T> {
+    constructor(public collection: T[]) {}
+
+    get(index: number): T {
+        return this.collection[index];
+    }
+}
+
+new ArrayOfAnything<string>(['a', 'b', 'c']);
+
+// EXAMPLES OF GENERICS WITH FUNCTIONS
+function printStrings(arr: string[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+function printNumbers(arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+function printAnything<T>(arr: T[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+printAnything<string>(['a', 'b', 'c']);
+
+// GENERIC CONSTRAINTS
+class Car {
+    print(): void {
+        console.log('I am a car');
+    }
+}
+
+class House {
+    print(): void {
+        console.log('I am a house');
+    }
+}
+
+interface Printable {
+    print(): void;
+}
+
+function printHousesOrCar<T extends Printable>(arr: T[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].print();
+    }
+}
