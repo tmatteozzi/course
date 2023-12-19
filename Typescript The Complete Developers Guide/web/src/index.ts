0e03e8b0bcd3ac7d2ee112257219fe4b8de09cf7
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2023-12-19 15:25:06 -0300

    Attributes class creation & integration

--- Typescript The Complete Developers Guide/web/src/index.ts
+++ Typescript The Complete Developers Guide/web/src/index.ts
@@ -1,9 +1,2 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'new record', age: 0 });
-
-user.events.on('change', () => {
-    console.log('CHANGE!');
-});
-
-user.events.trigger('change');
