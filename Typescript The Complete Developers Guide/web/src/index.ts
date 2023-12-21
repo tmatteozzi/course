14e0ac46ab7264dd21eafaef6d7506978d7393b7
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2023-12-21 19:50:09 -0300

    Events & RandomAge method

--- Typescript The Complete Developers Guide/web/src/index.ts
+++ Typescript The Complete Developers Guide/web/src/index.ts
@@ -1,5 +1,8 @@
+import { User } from './models/User';
 import { UserForm } from './views/UserForm';
 
-const userform = new UserForm(document.getElementById('root')!);
+const user = User.buildUser({ name: 'NAME', age: 20 });
+
+const userform = new UserForm(document.getElementById('root')!, user);
 
 userform.render();
