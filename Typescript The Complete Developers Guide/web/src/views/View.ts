be75083eaa5f8f0062627d41d658a180a067779c
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-01-02 13:37:36 -0300

    View Class

---
+++ Typescript The Complete Developers Guide/web/src/views/View.ts
@@ -0,0 +1,36 @@
+import { Model, HasId } from '../models/Model';
+
+export abstract class View<T extends Model<K>, K extends HasId> {
+    constructor(public parent: Element, public model: T) {
+        this.bindModel();
+    }
+
+    abstract eventsMap(): { [key: string]: () => void };
+    abstract template(): string;
+
+    bindModel(): void {
+        this.model.on('change', () => {
+            this.render();
+        });
+    }
+
+    bindEvents(fragment: DocumentFragment): void {
+        const eventsMap = this.eventsMap();
+        for (let eventKey in eventsMap) {
+            const [eventName, selector] = eventKey.split(':');
+
+            fragment.querySelectorAll(selector).forEach((element) => {
+                element.addEventListener(eventName, eventsMap[eventKey]);
+            });
+        }
+    }
+
+    render(): void {
+        // START PARENT EMPTY
+        this.parent.innerHTML = '';
+        const templateElement = document.createElement('template');
+        templateElement.innerHTML = this.template();
+        this.bindEvents(templateElement.content);
+        this.parent.appendChild(templateElement.content);
+    }
+}
