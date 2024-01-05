b7222b6fdce13b37acde05e2fc2ebd5d13dc313f
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-01-05 12:12:26 -0300

    Login Route

--- Typescript The Complete Developers Guide/server/src/index.ts
+++ Typescript The Complete Developers Guide/server/src/index.ts
@@ -1,1 +1,17 @@
-console.log('hi there');
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+
+// CREATE APP
+const app = express();
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ROUTER
+app.use(router);
+
+// APP LISTENING PORT
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
