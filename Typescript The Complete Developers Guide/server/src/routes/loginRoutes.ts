3192f1edfc10c423aa4c127f4ae50eaa06bcb38a
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-01-07 19:23:34 -0300

    Protected route

--- Typescript The Complete Developers Guide/server/src/routes/loginRoutes.ts
+++ Typescript The Complete Developers Guide/server/src/routes/loginRoutes.ts
@@ -1,7 +1,17 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
     body: { [key: string]: string | undefined };
+}
+
+// REQUIRE AUTH MIDDLEWARE
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not permitted');
 }
 
 const router = Router();
@@ -62,6 +72,10 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/logout', (req: Request, res: Response) => {
     req.session = undefined;
     res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+    res.send('Welcome to protected route, logged in user');
 });
 
 export { router };
