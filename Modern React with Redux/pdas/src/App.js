9c87cbaebdf1a9becf4a8187ea8f8a20eaeea0fd
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-01-26 16:02:22 -0300

    Images

--- Modern React with Redux/pdas/src/App.js
+++ Modern React with Redux/pdas/src/App.js
@@ -1,12 +1,21 @@
 import ProfileCard from './ProfileCard';
 
+// IMAGES
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
 function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+            />
+            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
         </div>
     );
 }
--- Modern React with Redux/pdas/src/ProfileCard.js
+++ Modern React with Redux/pdas/src/ProfileCard.js
@@ -1,5 +1,12 @@
-function ProfileCard() {
-    return <div>Profile Card</div>;
+function ProfileCard({ title, handle, image }) {
+    // PROPS HAS TITLE & HANDLE -> const {title, handle } = props;
+    return (
+        <div>
+            <img src={image} alt="pda logo" />
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+        </div>
+    );
 }
 
 export default ProfileCard;
