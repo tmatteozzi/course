2ef80d0774380a44cad262484251cc762dbdb15c
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-02-03 14:47:02 -0300

    BookList final save

--- Modern React with Redux/books/src/components/BookList.js
+++ Modern React with Redux/books/src/components/BookList.js
@@ -2,7 +2,14 @@ import BookShow from './BookShow';
 
 function BookList({ books, onDelete, onEdit }) {
     const renderedBooks = books.map((book) => {
-        return <BookShow onDelete={onDelete} onEdit={onEdit} key={book.id} book={book} />;
+        return (
+            <BookShow
+                onDelete={onDelete}
+                onEdit={onEdit}
+                key={book.id}
+                book={book}
+            />
+        );
     });
     return <div className="book-list">{renderedBooks}</div>;
 }
