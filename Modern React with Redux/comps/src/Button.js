434e07dc0522005281af5115c47721dd62f142f7
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-02-08 14:16:50 -0300

    Finished Button Component

--- Modern React with Redux/comps/src/Button.js
+++ Modern React with Redux/comps/src/Button.js
@@ -9,10 +9,11 @@ function Button({
     warning,
     danger,
     outline,
-    rounded
+    rounded,
+    ...rest
 }) {
     const classes = twMerge(
-        className('flex items-center px-3 py-1.5 border', {
+        className(rest.className, 'flex items-center px-3 py-1.5 border', {
             'border-blue-500 bg-blue-500 text-white': primary,
             'border-gray-900 bg-gray-900 text-white': secondary,
             'border-green-500 bg-green-500 text-white': success,
@@ -28,7 +29,11 @@ function Button({
         })
     );
 
-    return <button className={classes}>{children}</button>;
+    return (
+        <button {...rest} className={classes}>
+            {children}
+        </button>
+    );
 }
 
 Button.propTypes = {
