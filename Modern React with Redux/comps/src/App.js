a75795a00eef3928fbaff7c0188c6acb0751f551
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-02-08 15:18:08 -0300

    Accordion component

--- Modern React with Redux/comps/src/App.js
+++ Modern React with Redux/comps/src/App.js
@@ -1,35 +1,28 @@
-import Button from './components/Button';
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+import Accordion from './components/Accordion';
 
 function App() {
-    return (
-        <div>
-            <div>
-                <Button primary>
-                    <GoBell />
-                    Click me!
-                </Button>
-            </div>
-            <div>
-                <Button secondary>
-                    <GoCloudDownload />
-                    Buy Now!
-                </Button>
-            </div>
-            <div>
-                <Button success>
-                    <GoDatabase />
-                    See Deal!
-                </Button>
-            </div>
-            <div>
-                <Button warning>Hide Ads!</Button>
-            </div>
-            <div>
-                <Button danger>Something</Button>
-            </div>
-        </div>
-    );
+    const items = [
+        {
+            id: 'g1s4dj',
+            label: 'Can I use React in a project?',
+            content:
+                'You can use React in any project you want! You can use React in any project you want! You can use React in any project you want!'
+        },
+        {
+            id: 'h531hi',
+            label: 'Can I use JavaScript in a project?',
+            content:
+                'You can use JS in any project you want! You can use JS in any project you want! You can use JS in any project you want!'
+        },
+        {
+            id: 'hs8h3j',
+            label: 'Can I use CSS in a project?',
+            content:
+                'You can use CSS in any project you want! You can use CSS in any project you want! You can use CSS in any project you want!'
+        }
+    ];
+
+    return <Accordion items={items} />;
 }
 
 export default App;
