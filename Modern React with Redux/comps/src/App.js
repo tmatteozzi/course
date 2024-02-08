facc49bca9cb11e10c7b2d9bc49a615460b9dcac
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-02-08 13:07:02 -0300

    Adding icons to buttons

--- Modern React with Redux/comps/src/App.js
+++ Modern React with Redux/comps/src/App.js
@@ -1,16 +1,26 @@
 import Button from './Button';
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 
 function App() {
     return (
         <div>
             <div>
-                <Button primary>Click me!</Button>
+                <Button primary>
+                    <GoBell />
+                    Click me!
+                </Button>
             </div>
             <div>
-                <Button secondary>Buy Now!</Button>
+                <Button secondary>
+                    <GoCloudDownload />
+                    Buy Now!
+                </Button>
             </div>
             <div>
-                <Button success>See Deal!</Button>
+                <Button success>
+                    <GoDatabase />
+                    See Deal!
+                </Button>
             </div>
             <div>
                 <Button warning>Hide Ads!</Button>
