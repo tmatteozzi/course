997f335fdf65a5a34c965ca82f32de975180316e
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-02-15 11:49:35 -0300

    Link finished + Styling

--- Modern React with Redux/comps/src/components/Link.js
+++ Modern React with Redux/comps/src/components/Link.js
@@ -1,15 +1,26 @@
 import { useContext } from 'react';
+import classNames from 'classnames';
 import NavigationContext from '../context/navigation';
 
 function Link({ to, children }) {
     const { navigate } = useContext(NavigationContext);
 
+    const classes = classNames('text-blue-500');
+
     const handleClick = (event) => {
+        // CTRL + LINK CLICK TO OPEN LINK ON A NEW TAB
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
         event.preventDefault();
         navigate(to);
     };
 
-    return <a onClick={handleClick}>{children}</a>;
+    return (
+        <a className={classes} href={to} onClick={handleClick}>
+            {children}
+        </a>
+    );
 }
 
 export default Link;
