01fa04a8ce7a2b9a6f29d2b9fff14233a905652e
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-02-15 12:10:10 -0300

    Sidebar implementation + Styling

--- Modern React with Redux/comps/src/App.js
+++ Modern React with Redux/comps/src/App.js
@@ -1,19 +1,23 @@
-import Link from './components/Link';
 import Route from './components/Route';
+import SideBar from './components/Sidebar';
+
 import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
+import ButtonPage from './pages/ButtonPage';
 
 function App() {
     return (
-        <div>
-            <Link to={'/accordion'}>Go to accordion</Link>
-            <Link to={'/dropdown'}>Go to dropdown</Link>
-            <div>
+        <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+            <SideBar />
+            <div className="col-span-5">
+                <Route path="/">
+                    <DropdownPage />
+                </Route>
                 <Route path="/accordion">
                     <AccordionPage />
                 </Route>
-                <Route path="/dropdown">
-                    <DropdownPage />
+                <Route path="/buttons">
+                    <ButtonPage />
                 </Route>
             </div>
         </div>
