363d8b6f09e3bad2856ce602903d3eda1b966f84
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-02-16 14:10:58 -0300

    Extract useSort hook from SortableTable

--- Modern React with Redux/comps/src/components/SortableTable.js
+++ Modern React with Redux/comps/src/components/SortableTable.js
@@ -1,52 +1,14 @@
-import { useState } from 'react';
 import { GoArrowSmallUp, GoArrowSmallDown } from 'react-icons/go';
 import Table from './Table';
+import useSort from '../hooks/use-sort';
 
 function SortableTable(props) {
-    const [sortOrder, setSortOrder] = useState(null);
-    const [sortBy, setSortBy] = useState(null);
-
     const { config, data } = props;
 
-    const handleClick = (label) => {
-        // CHANGING SORT LABEL
-        if (sortBy && sortBy !== label) {
-            setSortOrder('asc');
-            setSortBy(label);
-            return;
-        }
-
-        // SORT ORDER CICLE
-        if (sortOrder === null) {
-            setSortOrder('asc');
-            setSortBy(label);
-        } else if (sortOrder === 'asc') {
-            setSortOrder('desc');
-            setSortBy(label);
-        } else if (sortOrder === 'desc') {
-            setSortOrder(null);
-            setSortBy(null);
-        }
-    };
-
-    // SORTING
-    let sortedData = data;
-    if (sortOrder && sortBy) {
-        const { sortValue } = config.find((column) => column.label === sortBy);
-
-        sortedData = [...data].sort((a, b) => {
-            const valueA = sortValue(a);
-            const valueB = sortValue(b);
-
-            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
-            // STRING
-            if (typeof valueA === 'string') {
-                return valueA.localeCompare(valueB) * reverseOrder;
-            }
-            // NUMBER
-            return (valueA - valueB) * reverseOrder;
-        });
-    }
+    const { sortOrder, sortBy, sortedData, setSortColumn } = useSort(
+        data,
+        config
+    );
 
     const updatedConfig = config.map((column) => {
         if (!column.sortValue) {
@@ -58,7 +20,7 @@ function SortableTable(props) {
             header: () => (
                 <th
                     className="cursor-pointer hover:bg-gray-100"
-                    onClick={() => handleClick(column.label)}
+                    onClick={() => setSortColumn(column.label)}
                 >
                     <div className="flex items-center">
                         {getIcons(column.label, sortBy, sortOrder)}
