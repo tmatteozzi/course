9a48b5d3e8a045c046060f6526ad69677e2aea4a
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-02-20 22:17:53 -0300

    CarList final implementation

--- Modern React with Redux/cars/src/components/CarList.js
+++ Modern React with Redux/cars/src/components/CarList.js
@@ -1,23 +1,42 @@
-import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { createSelector } from 'reselect';
 import { removeCar } from '../store';
 
 function CarList() {
     const dispatch = useDispatch();
 
-    const cars = useSelector(({ cars: { data, searchTerm } }) => {
-        // FILTER LOGIC
-        return data.filter((car) =>
-            car.name.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-    }, shallowEqual);
+    const { cars, name } = useSelector(
+        createSelector(
+            // INPUT SELECTORS
+            (state) => state.form.name,
+            (state) => state.cars.data,
+            (state) => state.cars.searchTerm,
+            // Memoized selector function
+            (name, cars, searchTerm) => {
+                // FILTER LOGIC
+                const filteredCars = cars.filter((car) =>
+                    car.name.toLowerCase().includes(searchTerm.toLowerCase())
+                );
+
+                return {
+                    cars: filteredCars,
+                    name: name
+                };
+            }
+        )
+    );
 
     const handleCarDelete = (car) => {
         dispatch(removeCar(car.id));
     };
 
     const renderedCars = cars.map((car) => {
+        // DECIDE IF THIS CAR SHOULD BE BOLD
+        const bold =
+            name && car.name.toLowerCase().includes(name.toLowerCase());
+
         return (
-            <div key={car.id} className="panel">
+            <div key={car.id} className={`panel ${bold ? 'bold' : ''}`}>
                 <p>
                     {car.name} - ${car.cost}
                 </p>
