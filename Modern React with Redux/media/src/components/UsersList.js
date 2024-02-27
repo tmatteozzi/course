95a358f0229d676b6bcb899deafb79394dc2d8ad
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-02-27 17:38:06 -0300

    Content change

--- Modern React with Redux/media/src/components/UsersList.js
+++ Modern React with Redux/media/src/components/UsersList.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { fetchUsers, addUser } from '../store';
+
 import Button from './Button';
 import Skeleton from './Skeleton';
 import useThunk from '../hooks/useThunk';
@@ -22,34 +23,33 @@ function UsersList() {
         doCreateUser();
     };
 
+    let content;
     if (isLoadingUsers) {
-        return <Skeleton times={6} className="h-10 w-full" />;
-    }
-
-    if (loadingUsersError) {
-        return <div>Error fetching data...</div>;
-    }
-
-    const renderedUsers = data.map((user) => {
-        return (
-            <div key={user.id} className="mb-2 border rounded">
-                <div className="flex p-2 justify-between items-center cursor-pointer">
-                    {user.name}
+        content = <Skeleton times={6} className="h-10 w-full" />;
+    } else if (loadingUsersError) {
+        content = <div>Error fetching data...</div>;
+    } else {
+        content = data.map((user) => {
+            return (
+                <div key={user.id} className="mb-2 border rounded">
+                    <div className="flex p-2 justify-between items-center cursor-pointer">
+                        {user.name}
+                    </div>
                 </div>
-            </div>
-        );
-    });
+            );
+        });
+    }
 
     return (
         <div>
-            <div className="flex flex-row justify-between m-3">
+            <div className="flex flex-row justify-between items-center m-3">
                 <h1 className="m-2 text-xl">Users</h1>
                 <Button onClick={handleUserAdd} loading={isCreatingUser}>
                     + Add User
                 </Button>
                 {creatingUserError && 'Error creating user'}
             </div>
-            {renderedUsers}
+            {content}
         </div>
     );
 }
