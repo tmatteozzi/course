5362533d8894287798a2afef1780409841dfaf3c
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-03-06 21:29:18 -0300

    Location Search finished

--- Modern React with Redux/location-search/src/App.tsx
+++ Modern React with Redux/location-search/src/App.tsx
@@ -1,5 +1,21 @@
+import LocationSearch from './components/LocationSearch';
+import Map from './components/Map';
+import type { Place } from './api/Place';
+import { useState } from 'react';
+
 function App() {
-    return <div>Hi there!</div>;
+    const [place, setPlace] = useState<Place | null>(null);
+
+    return (
+        <div className="h-screen w-screen grid grid-cols-12">
+            <div className="col-span-3 p-2">
+                <LocationSearch onPlaceClick={(p) => setPlace(p)} />
+            </div>
+            <div className="col-span-9">
+                <Map place={place} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
