03f3e7cfcbe0c6c798eb5270d3e6542054f334ba
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-03-08 15:38:19 -0300

    PackageListItem creation

---
+++ Modern React with Redux/registry/src/components/PackageListItem.tsx
@@ -0,0 +1,43 @@
+import { Link } from 'react-router-dom';
+import type { PackageSummary } from '../api/types/packageSummary';
+
+interface PackageListItemProps {
+    pack: PackageSummary;
+}
+
+// pack => package
+export default function PackageListItem({ pack }: PackageListItemProps) {
+    const renderedKeywords = (pack.keywords || []).map((keyword) => {
+        return (
+            <div
+                className="border py-0.5 px-1 text-xs bg-slate-200 rounded"
+                key={keyword}
+            >
+                {keyword}
+            </div>
+        );
+    });
+
+    return (
+        <div className="border p-4 rounded flex justify-between items-center">
+            <div className="flex flex-col gap-2">
+                <Link
+                    to={`packages/${pack.name}`}
+                    className="text-xl font-bold"
+                >
+                    {pack.name}
+                </Link>
+                <p className="text-sm text-gray-500">{pack.description}</p>
+                <div className="flex gap-1">{renderedKeywords}</div>
+            </div>
+            <div className="mr-6">
+                <Link
+                    to={`packages/${pack.name}`}
+                    className="py-2 px-3 rounded bg-black text-white text-lg"
+                >
+                    View
+                </Link>
+            </div>
+        </div>
+    );
+}
