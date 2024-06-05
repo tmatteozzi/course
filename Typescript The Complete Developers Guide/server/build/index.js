e606d3781a54401aefe0badef89f9859be939d33
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-06-05 18:36:51 -0300

    Password Generator App

--- Typescript The Complete Developers Guide/server/build/index.js
+++ Typescript The Complete Developers Guide/server/build/index.js
@@ -3,18 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const loginRoutes_1 = require("./routes/loginRoutes");
-const body_parser_1 = __importDefault(require("body-parser"));
-const cookie_session_1 = __importDefault(require("cookie-session"));
+var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var cookie_session_1 = __importDefault(require("cookie-session"));
+var AppRouter_1 = require("./AppRouter");
+require("./controllers/LoginController");
+require("./controllers/RootController");
 // CREATE APP
-const app = (0, express_1.default)();
+var app = (0, express_1.default)();
 // MIDDLEWARE
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_session_1.default)({ keys: ['lasfjaf'] })); // ADD SESSION TO REQ PROPERTY
 // ROUTER
-app.use(loginRoutes_1.router);
+app.use(AppRouter_1.AppRouter.getInstance());
 // APP LISTENING PORT
-app.listen(3000, () => {
+app.listen(3000, function () {
     console.log('Listening on port 3000');
 });
