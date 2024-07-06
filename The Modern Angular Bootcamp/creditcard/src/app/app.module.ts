d56ae969713663f96b2e4e0deeb4a948e6242898
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-07-06 20:11:52 -0300

    Credit Card App

---
+++ The Modern Angular Bootcamp/creditcard/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {CardFormComponent} from './card-form/card-form.component';
+import {InputComponent} from './input/input.component';
+import { CardComponent } from './card/card.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardFormComponent,
+    InputComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ The Modern Angular Bootcamp/creditcard/src/app/card-form/card-form.component.ts
@@ -0,0 +1,39 @@
+import {Component} from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {DateFormControl} from "../date-form-control";
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrl: './card-form.component.css'
+})
+export class CardFormComponent {
+  cardForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16)
+    ]),
+    expiration: new DateFormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3)
+    ])
+  })
+
+  onSubmit() {
+    console.log('Form Submitted')
+  }
+
+  onResetClick() {
+    this.cardForm.reset()
+  }
+}
