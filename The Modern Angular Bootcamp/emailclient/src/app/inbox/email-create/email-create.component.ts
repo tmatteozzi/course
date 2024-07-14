c9c555fb3ac7f18573f8401c96e1adb204c39947
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-07-14 12:17:22 -0300

    Sending Emails & Modal

--- The Modern Angular Bootcamp/emailclient/src/app/inbox/email-create/email-create.component.ts
+++ The Modern Angular Bootcamp/emailclient/src/app/inbox/email-create/email-create.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Email} from "../email";
+import {AuthService} from "../../auth/auth.service";
+import {EmailService} from "../email.service";
 
 @Component({
   selector: 'app-email-create',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './email-create.component.css'
 })
 export class EmailCreateComponent {
+  showModal = false;
+  email: Email = {
+    id: '',
+    to: '',
+    subject: '',
+    html: '',
+    text: '',
+    from: `${this.authService.username}@angular-email.com`
+  };
 
+  constructor(private authService: AuthService, private emailService: EmailService) {
+  }
+
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email)
+      .subscribe(() => {
+        this.showModal = false;
+      });
+  }
 }
